6beb71d66685606c523b31eba6ef8f1647f130c9
Individual IT Services <info@individual-it.net>
2015-08-06 16:17:51 +0545

    read and parse config file

--- owncloud-batch-tool.py
+++ owncloud-batch-tool.py
@@ -1,7 +1,24 @@
-#get the configuration file
+def read_config_parameter (name,mandatory=False):
+    try:
+        return config.get('DEFAULT', name);
+    except ConfigParser.NoOptionError:
+        if mandatory is True:
+            exit(name + " argument is missing")
+        else:
+            return;
+
 import ConfigParser
+import argparse
+
+parser = argparse.ArgumentParser(description='OwnCloud Batch Tool')
+parser.add_argument('--config', dest='configFile', default="config.cfg",  
+                   help='path of the configuration file')
+
+args = parser.parse_args()
+print args.configFile
 
 config = ConfigParser.ConfigParser()
-config.read('config.cfg')
-print config.get('DEFAULT', 'URL')
-print config.get('DEFAULT', 'adminUser')
+config.read(args.configFile)
+
+print read_config_parameter ('URL',True)
+print read_config_parameter ('adminUser',True)
