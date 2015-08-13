91d6f44ec770d7b0127be05fccda19c4db769f41
Individual IT Services <info@individual-it.net>
2015-08-13 16:45:21 +0545

    add users to groups according to the domain name of the username

--- owncloud-batch-tool.py
+++ owncloud-batch-tool.py
@@ -1,6 +1,13 @@
-def read_config_parameter (name,mandatory=False):
+def read_config_parameter (name,mandatory=False,type="text"):
     try:
-        return config.get('DEFAULT', name);
+        if type=="int":
+            return config.getint('DEFAULT', name);
+        elif type=="float":
+            return config.getfloat('DEFAULT', name);
+        elif type=="boolean":
+            return config.getboolean('DEFAULT', name);
+        else:
+            return config.get('DEFAULT', name);
     except ConfigParser.NoOptionError:
         if mandatory is True:
             exit(name + " argument is missing")
@@ -24,8 +31,22 @@ config.read(args.configFile)
 
 print read_config_parameter ('URL',True)
 print read_config_parameter ('adminUser',True)
-
+read_config_parameter("URL")
 oc = owncloud.Client(read_config_parameter ('URL',True))
 
 oc.login(read_config_parameter ('adminUser',True), read_config_parameter ('adminPassword',True))
+
+users = oc.search_users("")
+
+if read_config_parameter("groupsByDomainName",True,"boolean") is True:
+    print "groups by domain name"
+    for user in users:
+        # print user.text
+        email=user.text.split("@")
+        if len(email) > 1:
+            groups = email[1].split(".")
+            lastGroupNum=len(groups)-read_config_parameter("groupsByDomainNameSkipDomains",True,"int")
+            for group in groups[:lastGroupNum]:
+                print "user:"+user.text+"-"+group
+                oc.add_user_to_group(user.text,group)
 
