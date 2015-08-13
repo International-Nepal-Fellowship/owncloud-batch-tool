c4dd613f8efdd934aa68397bc11a8dabd11c0b32
Individual IT Services <info@individual-it.net>
2015-08-13 14:13:12 +0545

    connect to OwnCloud

--- owncloud-batch-tool.py
+++ owncloud-batch-tool.py
@@ -9,6 +9,8 @@ def read_config_parameter (name,mandatory=False):
 
 import ConfigParser
 import argparse
+import owncloud
+
 
 parser = argparse.ArgumentParser(description='OwnCloud Batch Tool')
 parser.add_argument('--config', dest='configFile', default="config.cfg",  
@@ -22,3 +24,8 @@ config.read(args.configFile)
 
 print read_config_parameter ('URL',True)
 print read_config_parameter ('adminUser',True)
+
+oc = owncloud.Client(read_config_parameter ('URL',True))
+
+oc.login(read_config_parameter ('adminUser',True), read_config_parameter ('adminPassword',True))
+
